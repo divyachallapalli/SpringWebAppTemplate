0c5deb491f726031468a514a21f28c0b24283c41
Divyakala <divyakala416@gmail.com>
2020-12-02 14:58:03 +0530

    Code changes to write output to file.

--- src/main/java/com/myapp/webservice/SuperMartWebServiceApplication.java
+++ src/main/java/com/myapp/webservice/SuperMartWebServiceApplication.java
@@ -1,15 +1,24 @@
 package com.myapp.webservice;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SuperMartWebServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SuperMartWebServiceApplication.class, args);
 		
 		System.out.println("Welcome to Fresh Super Mart...!");
+		File file=new File("test.txt");
+		String question = "HelloWorld";
+	     
+		Files.write(file.toPath(), question.getBytes());
 	}
 
 }
